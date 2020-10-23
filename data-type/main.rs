6b7f145e828443d1ecd3ac8a257375dd04acaef7
chandra agung rizky <needkopi@gmail.com>
2020-10-23 19:02:47 +0000

    add data-type

---
+++ data-type/main.rs
@@ -0,0 +1,75 @@
+fn main() {
+    /*
+     *  scalar type [integer, floating-point numbers, booleanm and character]
+     *  
+     * */
+
+    /*
+     * integer
+     *
+     * u8
+     * u16
+     * u32
+     * u64
+     * u128
+     * usize
+     *
+     * */
+
+    /*
+     * floating-point
+     *
+     * f32
+     * f64
+     *
+     * */
+
+    /*
+     * numeric operation
+     *
+     *  [+, -, *, /, %]
+     *
+     *  */
+
+    /*
+     * boolean [true, false]
+     * 
+     * */
+
+    /*
+     * character (char)
+     *
+     * example:
+     *  let a = 'a';
+     *
+     *
+     * */
+
+    /*
+     * compound type
+     *
+     * [tuple, array]
+     * */
+
+    /*
+     * tuple
+     *
+     * simplenya gruping dari beberapa value
+     *
+     * example
+     *  let tup: (u43, f64, u8) = (500, 6.4, 1);
+     * */
+
+    /*
+     * array
+     *
+     * berbeda dengan tuple, element setiap array harus type
+     * data yang sama
+     *
+     * example
+     *
+     * let arr = [1,2,3,4,5];
+     *
+     * */
+
+}
